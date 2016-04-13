ce1c330a99984a658ac560658d0968cb263d27cc
Andrew Ek <andrewek@gmail.com>
2016-04-13 10:51:22 -0500

    Full FlashCard model & validator tests

--- app/models/flash_card.rb
+++ app/models/flash_card.rb
@@ -3,5 +3,6 @@
 # ?? Category
 
 class FlashCard < ActiveRecord::Base
-  
+  validates :question, presence: true
+  validates :answer, presence: true
 end
--- tests/models/flash_card_test.rb
+++ tests/models/flash_card_test.rb
@@ -2,8 +2,21 @@ require_relative  "../test_helper"
 
 class FlashCardTest < Minitest::Test
   
-  def test_valid_at_creation
+  def test_invalid_without_question
     f = FlashCard.new(question: "This is a question?", answer: "This is an answer.")
     assert(f.valid?, "should be valid with a question and an answer")
+
+    f.question = nil
+    refute(f.valid?, "should be invalid without a question")
   end
+  
+  def test_invalid_without_answer
+    f = FlashCard.new(question: "This is a question?", answer: "This is an answer.")
+    assert(f.valid?, "should be valid with a question and an answer")
+
+    f.answer = nil
+    refute(f.valid?, "should be invalid without an answer")
+  end
+
+
 end
