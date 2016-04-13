c0fdcd52bc8650c3196d37c2f8a92d163e4ab0ee
Davida Gaffney <gdavida@gmail.com>
2016-04-13 15:29:44 -0500

    added AAG seed data and tested in pry

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,9 @@
 # each of your relevant models.
 #
 # You'll run it with rake db:seed
+
+AssignedAgeGroup.create(age:"0-3 months")
+AssignedAgeGroup.create(age:"3-6 months")
+AssignedAgeGroup.create(age:"6-12 months")
+AssignedAgeGroup.create(age:"12-24 months")
+AssignedAgeGroup.create(age:"3-5 years")
