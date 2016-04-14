d6267bd56df4c801e94b49a84d3b9e035f533535
Davida Gaffney <gdavida@gmail.com>
2016-04-14 10:32:54 -0500

    forgot to save model updates

--- app/models/toy.rb
+++ app/models/toy.rb
@@ -20,5 +20,19 @@
 
 
 class Toy < ActiveRecord::Base
-	
+  validates :name, presence: true
+  validates :brand, presence: true
+  validates :mfg_website, presence: true
+  validates :quantity_wanted, presence: true
+  validates :quantity_have, presence: true
+  validates :upc, presence: true, unique: true
+  validates :mfg_suggested_age_range, presence: true
+  validates :pieces, presence: true
+  validates :extended_play_idea
+  validates :store_purchased_from
+  validates :purchase_price, presence: true
+  validates :country_of_origin, presence: true
+  belongs_to :box
+  # has_and_belongs_to_many :skill
+  # has_and_belongs_to_many :assigned_age_group
 end
